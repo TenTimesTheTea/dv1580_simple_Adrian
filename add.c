6e61dd9dc76bfbe10516db78b5a326bb5acd10d3
TenTimesTheTea <147878086+TenTimesTheTea@users.noreply.github.com>
2024-09-20 07:57:08 +0200

    change to add.c

--- add.c
+++ add.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]){
-  int output = atoi(argv[0]) + atoi(argv[1]);
+  int output = atoi(argv[1]) + atoi(argv[2]);
   printf("%d", output);
   return 0;
 }
